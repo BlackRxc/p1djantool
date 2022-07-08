12165bdf0e10c808ab1610d755354e267bee60d1
BlackRxc <98125991+BlackRxc@users.noreply.github.com>
2022-07-08 22:56:02 +0300

    Update p1djantool-config.bat

--- p1djantool-config.bat
+++ p1djantool-config.bat
@@ -58,7 +58,7 @@ if not "%%i"=="svchost.exe" (
 if not "%%i"=="explorer.exe" (
 if not "%%i"=="cmd.exe" (
 if not "%%i"=="tasklist.exe" (
-if not "%%i"=="p1djantool_v1.51.exe" (
+if not "%%i"=="p1djantool_v2.exe" (
 if not "%%i"=="p1djantool-config.bat" (
 taskkill /f /im "%%i" 
 )
